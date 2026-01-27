8c5ab8671e614c9f0a4b188d93c012006655d740
Pedro <pedromsouza97@gmail.com>
2026-01-27 17:31:47 -0300

    Incluindo novas Rotas

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,18 +1,43 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import blogFetch from "../axios/config";
 import "./Home.css";
 
 const Home = () => {
   const [posts, setPosts] = useState([]);
 
   const getPosts = async () => {
-    console.log("Testando");
+    try {
+      const response = await blogFetch.get("/posts");
+
+      const data = response.data;
+      setPosts(data);
+    } catch (error) {
+      console.error("error");
+    }
   };
 
   useEffect(() => {
     getPosts();
   }, []);
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <h1>últimos posts</h1>
+      {posts.length === 0 ? (
+        <p>Carregando...</p>
+      ) : (
+        posts.map((post) => (
+          <div className="post" key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <Link to={`/post/${post.id}`} className="btn">
+              Ler mais
+            </Link>
+          </div>
+        ))
+      )}
+    </div>
+  );
 };
 
 export default Home;
